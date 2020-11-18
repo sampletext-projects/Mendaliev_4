b35fa415f30a76bc3d01ca88f56188938b10a520
Bird Egop <sampletext32@bk.ru>
2020-11-18 20:15:05 +0300

    Add project

---
+++ Mendaliev_4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Mendaliev_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //В зависимости от языка системы дробные числа с точкой не конвертируются из строк
+            //поэтому точки вручную заменяем на запятые
+            //.Replace(".", ",")
+
+            Console.WriteLine("Введите x: ");
+            double x = double.Parse(Console.ReadLine().Replace(".", ","));
+            Console.WriteLine("Введите a: ");
+            double a = double.Parse(Console.ReadLine().Replace(".", ","));
+            Console.WriteLine("Введите n: ");
+            double n = double.Parse(Console.ReadLine().Replace(".", ","));
+
+            double sinx = Math.Sin(x);
+
+            double x2N = x * x;
+            double summN = 5;
+            double sinxN = sinx * sinx;
+            int signN = 1;
+            double a2N = a * a;
+
+            double L = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                var val = signN * (x2N + summN) / Math.Sqrt(sinxN + a2N);
+                Console.WriteLine(val);
+                L += val;
+                x2N *= x * x;
+                sinxN *= sinx;
+                a2N *= a * a;
+                signN = -signN;
+            }
+
+            Console.WriteLine("L = {0:F}", L);
+        }
+    }
+}
